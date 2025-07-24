8cf3c0dceeb473e61670b79d0114eb2b4c06811b
Karthikeyan1096 <karthik0110@outlook.com>
2025-07-24 11:53:26 +0530

    Add files via upload

---
+++ login_page.py
@@ -0,0 +1,23 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+class LoginPage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.username_field = (By.ID, "txtUsername")
+        self.password_field = (By.ID, "txtPassword")
+        self.login_button = (By.ID, "btnLogin")
+        self.error_message = (By.ID, "spanMessage")
+
+    def enter_username(self, username):
+        self.driver.find_element(*self.username_field).send_keys(username)
+
+    def enter_password(self, password):
+        self.driver.find_element(*self.password_field).send_keys(password)
+
+    def click_login(self):
+        self.driver.find_element(*self.login_button).click()
+
+    def get_error_message(self):
+        return self.driver.find_element(*self.error_message).text
